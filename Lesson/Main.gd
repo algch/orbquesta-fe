97cd0b20de01c5c0e834900607deb623845bfbc1
3D <drarbego@gmail.com>
2020-08-30 20:49:40 -0500

    added pentagram

--- Lesson/Main.gd
+++ Lesson/Main.gd
@@ -7,7 +7,7 @@ var BEATS_PER_BAR = 4
 var UNITS_IN_BAR = BEATS_PER_BAR * MIN_NOTES_PER_BEAT * UNIT
 
 var selected_note_value = null
-var bars = [0]
+var bars = [[]]
 
 func _ready():
 	print("BAR UNIT SIZE ", UNITS_IN_BAR)
@@ -21,22 +21,45 @@ func _on_add_note_to_bar():
 		print("SELECT A NOTE!")
 		return
 
-	# add a list of nodes instead of the whole value
 	var remaining_units = selected_note_value
 	while remaining_units > 0:
 		var bar = bars[len(bars) - 1]
-		if bar + remaining_units >= UNITS_IN_BAR:
-			# wrong
-			bars[len(bars) - 1] = UNITS_IN_BAR
-			append_bar()
-		else:
-			bars[len(bars) - 1] = bar + remaining_units
 
-		remaining_units -= (UNITS_IN_BAR - bar)
+		var total_units_in_bar = 0
+		for u in bar:
+			if u is int:
+				total_units_in_bar += u
+
+		if total_units_in_bar + remaining_units >= UNITS_IN_BAR:
+			bar.append( (UNITS_IN_BAR - total_units_in_bar) )
+			append_bar()
+			remaining_units -= (UNITS_IN_BAR - total_units_in_bar)
+			if remaining_units:
+				bar.append("l")
+		else:
+			bar.append(remaining_units)
+			remaining_units -= (UNITS_IN_BAR - total_units_in_bar)
 
 	print("bars ", bars)
 
-
-
 func append_bar():
-	bars.append(0)
+	bars.append([])
+
+func _draw():
+	draw_pentagram()
+	draw_notes()
+
+func draw_pentagram():
+	var x_size = 500
+	var y_offset = 0
+	var y_delta = $HBoxContainer/TextureRect.texture.get_height() / 4.5
+	for i in range(0, 5):
+		draw_line($pentagram.position + Vector2(0, y_offset), $pentagram.position + Vector2(x_size, y_offset), Color(0, 0, 0))
+		y_offset += y_delta
+
+# TODO
+func draw_notes():
+	for bar in bars:
+		for note in bar:
+			if note is int:
+				pass

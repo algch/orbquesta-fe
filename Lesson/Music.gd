e40c7913ebf808f41df556fd3175bf239d7146bd
3D <drarbego@gmail.com>
2020-10-20 01:41:48 -0500

    added basic bar representation

--- Lesson/Music.gd
+++ Lesson/Music.gd
@@ -4,15 +4,14 @@ const BAR_SIZE = 200 # bar width in pixels
 
 
 class Note:
-	var quantity : int
-	var note_value : int
+	var value : int
 	var pitch : int setget set_pitch
 	var pitch_name
 	var is_rest : bool
 
-	func _init(note_value : int, pitch : int, rest : bool = false):
+	func _init(value : int, pitch : int, rest : bool = false):
 		# an integer N representing 1/N of a whole
-		self.note_value = note_value
+		self.value = value
 		self.pitch = pitch # 0 .. 120 like midi
 
 	func set_pitch(new_pitch):
@@ -28,9 +27,33 @@ class Note:
 
 class Bar:
 	var notes := []
+	var time_signature : TimeSignature
+	var width : float
+	var full := false
+	var space_left := 1
+	var previous : Bar
+	var next : Bar
+
+	func _init(signature : TimeSignature, w : float):
+		self.time_signature = signature
+		self.width = w
 
 	func add_note(note : Note):
-		self.notes.append(note)
+		if note_fits(note):
+			self.notes.append(note)
+
+	func note_fits(note : Note):
+		# at some point this is not going to work
+		# find an integer solution
+		var consumed_space = 0.0
+		for n in self.notes:
+			consumed_space += 1 / float(n.value)
+		consumed_space += 1 / float(note.value)
+
+		return consumed_space <= 1
+
+	func get_note_width(note : Note):
+		return width * (1 / float(note.value))
 
 class TimeSignature:
 	var beats : int
--- Lesson/test/test_music.gd
+++ Lesson/test/test_music.gd
@@ -3,5 +3,22 @@ extends "res://addons/gut/test.gd"
 var Music = preload("res://Music.gd")
 
 func test_note_generates_expected_pitch_name():
-	var a5 = Music.Note.new(0, 60)
+	var a5 = Music.Note.new(1, 60)
 	assert_eq(a5.pitch_name, "A 5")
+
+func test_bar_calculates_expected_note_width():
+	var note = Music.Note.new(2, 0)
+	var signature = Music.TimeSignature.new(4, 4)
+	var bar_width = 200.0
+	var bar = Music.Bar.new(signature, bar_width)
+	assert_eq(int(bar.get_note_width(note)), 100)
+
+func test_bar_calculates_space_left():
+	var signature = Music.TimeSignature.new(4, 4)
+	var bar_width = 200.0
+	var bar = Music.Bar.new(signature, bar_width)
+	for i in range(15):
+		var note = Music.Note.new(16, 0)
+		bar.add_note(note)
+	assert_false(bar.note_fits(Music.Note.new(8, 0)))
+	assert_true(bar.note_fits(Music.Note.new(16, 0)))

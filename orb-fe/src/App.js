b1e6b3d6edbeec3afdf0b9a1ae5425c6ce029e22
Alejandro III de Macedonia <alejandro@Alejandros-MacBook-Pro.local>
2020-10-31 19:13:21 -0600

    add basic routing

--- orb-fe/src/App.js
+++ orb-fe/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import RythmicDictation from './scenes/RythmicDictation';
+import Login from './scenes/Login';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        Orbquesta
-      </div>
-      <RythmicDictation />
-    </div>
+    <BrowserRouter>
+        <Route exact path="/" >
+          <Login />
+        </Route>
+        <Route exact path="/exercises" >
+          <RythmicDictation />
+        </Route>
+    </BrowserRouter>
   );
 }
 
---
+++ orb-fe/src/scenes/Login.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Login() {
+    return (
+        <div>
+            Login
+        </ div>
+    );
+
+}
+
+export default Login;
